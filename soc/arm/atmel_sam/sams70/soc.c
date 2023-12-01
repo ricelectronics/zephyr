f608370049a5ececbc8081ca0c71570fe77d6601
Marius Scholtz <mariuss@ricelectronics.com>
2023-12-01 13:05:12 -0800

    initial fixes for zephyr-3.5

--- soc/arm/atmel_sam/sams70/soc.c
+++ soc/arm/atmel_sam/sams70/soc.c
@@ -15,9 +15,8 @@
 #include <zephyr/device.h>
 #include <zephyr/init.h>
 #include <soc.h>
-#include <zephyr/arch/arm/aarch32/cortex_m/cmsis.h>
+#include <cmsis_core.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/irq.h>
 
 #define LOG_LEVEL CONFIG_SOC_LOG_LEVEL
 LOG_MODULE_REGISTER(soc);
